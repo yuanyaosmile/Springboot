1f1c4de26e46ee5c47f45c49001152f4a9efd041
yuanyao <yuanyaosmile@gmail.ocm>
2018-11-09 23:17:38 +0800

    user entity

--- springboot-demo/src/main/java/com/example/entity/User.java
+++ springboot-demo/src/main/java/com/example/entity/User.java
@@ -2,12 +2,14 @@ package com.example.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by yuanyao on 2018/10/29.
  */
 @Getter
 @Setter
+@Component
 public class User {
     public String username;
     public String email;
---
+++ springboot-demo/src/test/java/com/example/controller/HelloControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.controller;
+
+/**
+ * Created by yuanyao on 2018/11/9.
+ */
+public class HelloControllerTest {
+}
