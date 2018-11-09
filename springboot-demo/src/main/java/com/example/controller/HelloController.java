b734b784b87ddfb2f35a34e0f77555605f94b88d
yuanyao <yuanyaosmile@gmail.ocm>
2018-11-09 23:31:24 +0800

    add controller

--- springboot-demo/src/main/java/com/example/controller/HelloController.java
+++ springboot-demo/src/main/java/com/example/controller/HelloController.java
@@ -1,7 +1,21 @@
 package com.example.controller;
 
+import com.example.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by yuanyao on 2018/11/9.
  */
+@RestController
+@RequestMapping("/user")
 public class HelloController {
+    @Autowired
+    User User ;
+
+    @RequestMapping("/index")
+    public String index() {
+        return "Hello world";
+    }
 }
