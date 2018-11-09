a1e931a9e2ecf71be609760675efd9473caf87d5
yuanyao <yuanyaosmile@gmail.ocm>
2018-11-09 23:32:56 +0800

    add testing case

--- springboot-demo/src/test/java/com/example/controller/HelloControllerTest.java
+++ springboot-demo/src/test/java/com/example/controller/HelloControllerTest.java
@@ -1,7 +1,38 @@
 package com.example.controller;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 /**
  * Created by yuanyao on 2018/11/9.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HelloControllerTest {
+
+    private MockMvc mvc;
+
+    @Before
+    public void init() {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void indexTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/user/index").accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello world")));
+    }
 }
