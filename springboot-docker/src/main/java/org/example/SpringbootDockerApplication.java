c5c93f558aeae48171653bbef98ce002b8489109
yuanyao <yuanyaosmile@gmail.com>
2020-12-25 20:02:08 +0800

    a demo project for springboot web application and it has a Dockerfile and we can use it to build an image

---
+++ springboot-docker/src/main/java/org/example/SpringbootDockerApplication.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerApplication.class, args);
+    }
+
+    @GetMapping("/hi")
+    public String sayHi() {
+        return "hi";
+    }
+}
