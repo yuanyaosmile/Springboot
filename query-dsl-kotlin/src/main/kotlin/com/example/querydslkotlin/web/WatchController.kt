6c66aa9f61ec1d9976396a4724c938c324a1d6ca
yuanyao <yuanyaosmile@gmail.com>
2022-12-04 11:19:13 +0800

    use jetty instead of tomcat

--- query-dsl-kotlin/src/main/kotlin/com/example/querydslkotlin/web/WatchController.kt
+++ query-dsl-kotlin/src/main/kotlin/com/example/querydslkotlin/web/WatchController.kt
@@ -7,6 +7,13 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileReader
+import java.io.OutputStreamWriter
 
 @RestController
 class WatchController @Autowired constructor(
@@ -19,5 +26,22 @@ class WatchController @Autowired constructor(
         @RequestParam color: String
     ): ResponseEntity<WatchEntity> {
         return ResponseEntity.ok(watchService.getWatch(brand, name, color))
+    }
+
+    @GetMapping("/hello")
+    fun getHello() {
+        var attributes = RequestContextHolder.getRequestAttributes() as ServletRequestAttributes
+        var httpServletResponse = attributes.response!!
+        val file = File("hello.csv")
+        BufferedReader(FileReader(file)).use { reader ->
+            BufferedWriter(OutputStreamWriter(httpServletResponse.outputStream, Charsets.UTF_8)).use { writer ->
+                var line:String?
+                var count:Int = 0
+                while (reader.readLine().also { line = it } != null) {
+                    count++
+                    writer.write("$count---$line\n")
+                }
+            }
+        }
     }
 }
